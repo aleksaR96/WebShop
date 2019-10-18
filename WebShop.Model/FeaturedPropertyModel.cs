7168ee0c8e8da460a7dcc816d0bd2884557187a0
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-18 11:25:31 +0200

    Dodata entitetska klasa za featured properties

---
+++ WebShop.Model/FeaturedPropertyModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Model
+{
+    public class FeaturedPropertyModel : BaseModel
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public int PropertyID { get; set; }
+
+        public FeaturedPropertyModel(int iD, int categoryID, int propertyID)
+        {
+            ID = iD;
+            CategoryID = categoryID;
+            PropertyID = propertyID;
+        }
+
+        public FeaturedPropertyModel(int iD)
+        {
+            ID = iD;
+        }
+
+        public FeaturedPropertyModel()
+        {
+        }
+    }
+}
