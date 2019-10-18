588cddc38916a519e8ae539c7d6e74063164c652
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-18 12:04:38 +0200

    Dodate 2 select metode u data sloju ya featured property

---
+++ WebShop.Data/FeaturedPropertyData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using WebShop.Interfaces;
+using WebShop.Model;
+
+namespace WebShop.Data
+{
+    class FeaturedPropertyData : BaseData, IFeaturedPropertyData
+    {
+        public List<FeaturedPropertyModel> SelectAllFeaturedProperties()
+        {
+            using (SqlConnection conn = base.createConnection())
+            {
+                SqlCommand sqlCommand =  base.createSqlCommandSP("SelectAllFeaturedProperties");
+
+                List<FeaturedPropertyModel> outputList = new List<FeaturedPropertyModel>();
+                FeaturedPropertyModel output;
+
+                SqlDataReader sqlDataReader = base.getSqlDataReader();
+
+                while(sqlDataReader.Read())
+                {
+                    output = new FeaturedPropertyModel();
+                    output.ID = Convert.ToInt32(sqlDataReader["ID"]);
+                    output.CategoryID = Convert.ToInt32(sqlDataReader["CategoryID"]);
+                    output.CategoryID = Convert.ToInt32(sqlDataReader["PropertyID"]);
+                    output.State = ModelState.Selected;
+                    outputList.Add(output);
+                }
+
+                base.closeConnection();
+                return outputList;
+            }
+        }
+
+        public FeaturedPropertyModel SelectFeaturedPropertyByID(FeaturedPropertyModel property)
+        {
+            using (SqlConnection conn = base.createConnection())
+            {
+                SqlCommand sqlCommand = base.createSqlCommandSP("SelectFeaturedPropertyByID");
+                sqlCommand.Parameters.Add(new SqlParameter("@ID", property.ID));
+
+                FeaturedPropertyModel output = new FeaturedPropertyModel();
+
+                SqlDataReader sqlDataReader = base.getSqlDataReader();
+
+                while(sqlDataReader.Read())
+                {
+                    output.ID = Convert.ToInt32(sqlDataReader["ID"]);
+                    output.CategoryID = Convert.ToInt32(sqlDataReader["CategoryID"]);
+                    output.PropertyID = Convert.ToInt32(sqlDataReader["PropertyID"]);
+                    output.State = ModelState.Selected;
+                }
+
+                base.closeConnection();
+                return output;
+            }
+        }
+        
+    }
+}
---
+++ WebShop.Interfaces/IFeaturedPropertyData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebShop.Model;
+
+namespace WebShop.Interfaces
+{
+    public interface IFeaturedPropertyData
+    {
+        List<FeaturedPropertyModel> SelectAllFeaturedProperties();
+
+        FeaturedPropertyModel SelectFeaturedPropertyByID(FeaturedPropertyModel property);
+    }
+}
