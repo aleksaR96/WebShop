3d84c6ca7a235b64a96b5d2ed2700679f134581b
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-22 14:05:16 +0200

    Dodat model za filterproperties

---
+++ WebShop.Model/FilterPropertiesModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Model
+{
+    public class FilterPropertiesModel
+    {
+        public int ID { get; set; }
+        public int CategoryID { get; set; }
+        public int PropertyID { get; set; }
+
+        public FilterPropertiesModel(int iD, int categoryID, int propertyID)
+        {
+            ID = iD;
+            CategoryID = categoryID;
+            PropertyID = propertyID;
+        }
+
+        public FilterPropertiesModel(int iD)
+        {
+            ID = iD;
+        }
+
+        public override string ToString()
+        {
+            return ID + " " + CategoryID + " " + PropertyID;
+        }
+    }
+}
