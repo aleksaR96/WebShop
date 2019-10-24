6150aa584efc6af1db47edec140d3de8a40c68ff
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-24 11:01:56 +0200

    Dodat test api

---
+++ WebShop.API/Controllers/ValuesController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebShop.Business;
+using WebShop.Model;
+
+namespace WebShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [Produces("application/json")]
+        public ActionResult Get(int id)
+        {
+            ProductsBusiness pb = new ProductsBusiness();
+            ProductsModel product = pb.GetProduct(new ProductsModel(id));
+            if (product.State == Model.ModelState.Selected)
+            {
+                return Ok(product);
+            }
+            else if (product.State == Model.ModelState.None)
+            {
+                return NotFound();
+            }
+
+            return BadRequest();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
