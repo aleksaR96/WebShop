1011f15dfcbc63cb1bf43cccc1cffd0ef398ba15
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-24 17:45:21 +0200

    Dodat korisnik apija koji je MVC

---
+++ WebShop.API/Controllers/AddProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.Business;
+
+namespace WebShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            CategoryBusiness cb = new CategoryBusiness();
+            return Ok(cb.GetAllCategories());
+        }
+    }
+}
---
+++ WebShop.KorisnikApija/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebShop.KorisnikApija.Models;
+
+namespace WebShop.KorisnikApija.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> LoadCategories()
+        {
+            var result = await GetCategoriesAsync("https://localhost:44315/api/AddProduct");
+            return Ok(result);
+        }
+
+        static async Task<string> GetCategoriesAsync(string path)
+        {
+            HttpClient client = new HttpClient();
+
+            HttpResponseMessage response = await client.GetAsync(path);
+
+            var result = await response.Content.ReadAsStringAsync();
+           
+            return result;
+        }
+    }
+}
