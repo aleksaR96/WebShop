2a3ff30977c7ce29c24d4fd9c1c0ce18e996a53b
praksa <praksa@BEL-MARMIL-LT01>
2019-10-25 13:04:23 +0200

    123

--- WebShop.UI.Console/Program.cs
+++ WebShop.UI.Console/Program.cs
@@ -13,7 +13,26 @@ namespace WebShop.UI.Console
     {
         static void Main(string[] args)
         {
-            
+
+            BrandsBusiness bb = new BrandsBusiness();
+            List<BrandModel> list = new List<BrandModel>();
+            list = bb.GetAllBrands();
+
+            foreach (BrandModel novbr in list)
+            {
+                WriteLine(novbr);
+            }
+
+            BrandModel novi = bb.GetBrand(new BrandModel(10));
+            WriteLine(novi);
+
+            var dodat = bb.AddBrand(new BrandModel("Toshiba"));
+            WriteLine(dodat);
+
+
+            var izmenjen = bb.EditBrand(new BrandModel(dodat.ID, "Vox"));
+            WriteLine(izmenjen);
+
             ReadKey();
 
         }
