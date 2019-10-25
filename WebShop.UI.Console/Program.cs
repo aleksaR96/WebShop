7a2451c9c2f95a572ec683c808efae2eaf5394e2
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-25 13:01:35 +0200

    asd

--- WebShop.UI.Console/Program.cs
+++ WebShop.UI.Console/Program.cs
@@ -13,27 +13,7 @@ namespace WebShop.UI.Console
     {
         static void Main(string[] args)
         {
-            BrandsData bd = new BrandsData();
-            var brands = bd.SelectAll();
-
-            foreach(BrandModel brd in brands)
-            {
-                WriteLine(brd);
-            }
-
-            BrandModel brand = bd.Insert(new BrandModel("Gorenje"));
-            WriteLine(brand + "\n");
-            brand = bd.Select(brand);
-            WriteLine(brand + "\n");
-            brand = bd.Update(new BrandModel(brand.ID, "Beko"));
-            WriteLine(brand + "\n");
-            bd.Delete(brand);
-
-            brands = bd.SelectAll();
-            foreach (BrandModel brd in brands)
-            {
-                WriteLine(brd);
-            }
+            
 
             ReadKey();
 
