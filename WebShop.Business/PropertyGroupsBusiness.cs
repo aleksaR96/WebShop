6c9889f1a345682024d76aa6c330fadf244e827b
praksa <praksa@BEL-MARMIL-LT01>
2019-10-25 14:56:14 +0200

    Dodat Business sloj za PropertyGroups

---
+++ WebShop.Business/PropertyGroupsBusiness.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using WebShop.Data;
+using WebShop.Interfaces;
+using WebShop.Model;
+
+namespace WebShop.Business
+{
+    public class PropertyGroupsBusiness
+    {
+
+        public List<PropertyGroupsModel> GetAllPropertyGroups()
+        {
+            IPropertyGroups pg = new PropertyGroupsData();
+            return pg.SelectAll();
+        }
+
+        public PropertyGroupsModel GetPropertyGroups(PropertyGroupsModel propertyGroups)
+        {
+            IPropertyGroups pg = new PropertyGroupsData();
+            return pg.Select(propertyGroups);
+        }
+
+        public PropertyGroupsModel AddPropertyGroups(PropertyGroupsModel propertyGroups)
+        {
+            IPropertyGroups pg = new PropertyGroupsData();
+            
+            PropertyGroupsModel output = null;
+
+            try
+            {
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    output = pg.Insert(propertyGroups);
+                    scope.Complete();
+                }
+            }
+            catch (TransactionAbortedException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            return output;
+        }
+
+        public PropertyGroupsModel EditPropertyGroups(PropertyGroupsModel propertyGroups)
+        {
+            IPropertyGroups pg = new PropertyGroupsData();
+            PropertyGroupsModel output = null;
+
+            try
+            {
+                using (TransactionScope scope = new TransactionScope())
+                {
+                    output = pg.Update(propertyGroups);
+                    scope.Complete();
+                }
+            }
+            catch (TransactionAbortedException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return output;
+        }
+       
+
+    }
+
+    
+}
