9f581fc88590df11ebd00ea01187c0db56b9b659
Aleksa Ristic <aleksa1996@gmail.com>
2019-10-31 10:49:30 +0100

    Dodat PropertyValueModel

---
+++ WebShop.Model/PropertyValueModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebShop.Model
+{
+    public class PropertyValueModel
+    {
+        public int ID { get; set; }
+        public int PropertyID { get; set; }
+        public string Value { get; set; }
+
+        public PropertyValueModel(int iD, int propertyID, string value)
+        {
+            ID = iD;
+            PropertyID = propertyID;
+            Value = value;
+        }
+
+        public PropertyValueModel(int propertyID, string value)
+        {
+            PropertyID = propertyID;
+            Value = value;
+        }
+
+        public PropertyValueModel(int iD)
+        {
+            ID = iD;
+        }
+
+        public PropertyValueModel()
+        {
+        }
+
+        public override string ToString()
+        {
+            return ID + " " + PropertyID + " " + Value;
+        }
+    }
+}
